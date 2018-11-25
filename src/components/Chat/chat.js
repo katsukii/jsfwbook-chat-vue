39412a745409fc7fd836fd39f061f0781b6b2953
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-11-26 03:24:12 +0900

    インスタンスがマウントされた直後にAPIからchannelsを呼び出す処理を追加

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -1,13 +1,19 @@
 import { mapGetters, mapActions } from 'vuex'
-import { SET_MESSAGE } from '../../store/mutation-types'
+import { 
+  GET_CHANNELS, 
+  SET_MESSAGE 
+} from '../../store/mutation-types'
 
 export default {
   name: 'chat', 
+  mounted() {
+    this.GET_CHANNELS()
+  }, 
   computed: {
-    ...mapGetters(['messages']), 
+    ...mapGetters(['messages', 'channels']), 
   }, 
   methods: {
-    ...mapActions([SET_MESSAGE]), 
+    ...mapActions([SET_MESSAGE, GET_CHANNELS]), 
     send_message() {
       this.SET_MESSAGE(this.message) // this.messages.push(this.message)
       this.message = ""
@@ -15,7 +21,6 @@ export default {
   }, 
   data() {
     return {
-      channels: ["general", "random"], 
       message: ""
     }
   }
