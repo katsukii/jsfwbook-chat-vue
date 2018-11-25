5fab18b2ca6f49f41d3177686cb42cf25326980d
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-11-25 11:35:22 +0900

    ajaxでデータをgetする。

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,7 +1,18 @@
-import { SET_MESSAGE } from './mutation-types'
+import {
+  SET_MESSAGE, 
+  GET_CHANNELS
+} from './mutation-types'
 
 export default {
   [SET_MESSAGE] ({commit}, message) {
     commit(SET_MESSAGE, message)
   }, 
+  [GET_CHANNELS] ({commit}) {
+    fetch('https://us-central1-demoapp-1779c.cloudfunctions.net/v1/channels')
+    .then((response) => {
+      return response.json()
+    }).then((json) => {
+      commit(GET_CHANNELS, json.channels)
+    })
+  }
 }
