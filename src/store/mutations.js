8f505c87a45dc01942a52ec84f9485a7edeb8f84
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-11-29 22:21:54 +0900

    SET_MESSAGESの追加

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,13 +1,17 @@
 import { 
-  SET_MESSAGE, 
-  GET_CHANNELS
+  // SET_MESSAGE, 
+  GET_CHANNELS, 
+  SET_MESSAGES
 } from './mutation-types'
 
 export default {
-  [SET_MESSAGE] (state, message) {
-    state.messages.push(message)
-  }, 
+  // [SET_MESSAGE] (state, message) {
+  //   state.messages.push(message)
+  // }, 
   [GET_CHANNELS] (state, channels) {
     state.channels = channels
+  }, 
+  [SET_MESSAGES] (state, messages) {
+    state.messages = messages
   }
 }
