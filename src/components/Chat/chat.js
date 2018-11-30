6651b92d2bd52eef99c25693e3650054522ab9a3
Yusuke Katsuki <katsuki.yusuke@gmail.com>
2018-12-01 03:23:52 +0900

    チャットコンポーネントにメッセージリストの追加

--- src/components/Chat/chat.js
+++ src/components/Chat/chat.js
@@ -1,23 +1,38 @@
 import { mapGetters, mapActions } from 'vuex'
 import { 
   GET_CHANNELS, 
-  SET_MESSAGE 
+  // SET_MESSAGE 
 } from '../../store/mutation-types'
+// コンポーネントをインポートする
+import MessageList from '../MessageList'
 
 export default {
   name: 'chat', 
   mounted() {
     this.GET_CHANNELS()
+    // メッセージを取得
+    this.GET_MESSAGES(this.$route.params.cname)
   }, 
   computed: {
     ...mapGetters(['messages', 'channels']), 
   }, 
   methods: {
-    ...mapActions([SET_MESSAGE, GET_CHANNELS]), 
+    ...mapActions([
+      // SET_MESSAGE, 
+      GET_CHANNELS, 
+      // GET_MESSAGESとPOST_MESSAGESをマッピング
+      "GET_MESSAGES", 
+      "POST_MESSAGES"
+    ]), 
     send_message() {
-      this.SET_MESSAGE(this.message) // this.messages.push(this.message)
+      // this.SET_MESSAGE(this.message) // this.messages.push(this.message)
+      // メッセージを送信
+      this.POST_MESSAGES({"cname": this.$route.params.cname, "message": this.message})
       this.message = ""
     }
+  },
+  components: {
+    'message-list': MessageList
   }, 
   data() {
     return {
